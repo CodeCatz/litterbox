84a368ecf4b5b68b3dfa056995283648708a246f
Sara Sabjan <sara.sabjan@gmail.com>
2013-07-21 19:44:20 +0200

    deleted old file, as Goran commanded

--- warnings_for_beginners.py
+++
@@ -1,3 +0,0 @@
-print ""A programmer will eventually tell you to use Mac OSX or Linux. If the programmer likes fonts and typography, he'll tell you to get a Mac OSX computer. If he likes control and have a huge beard, they'll tell you to install Linux.""
-
-I loled. You MAC users, you. :>
