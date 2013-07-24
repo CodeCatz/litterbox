269fb8c791a656c5aa9e361e463736a4faa2cff6
Pija Balaban <pijabalaban@Pija-Balabans-MacBook-Air.local>
2013-07-24 11:38:32 +0200

    Learn Python the Hard Way 1-13

---
+++ Homework1/ex7.py
@@ -0,0 +1,23 @@
+print "Mary had a little lamb." #napise besedilo
+print "Its fleece was white as %s." % 'snow' #napise besedilo in doda noter snow
+print "And everywhere that Mary went." #napise besedilo
+print "." * 10 # what'd that do? #naredi 10 pikic, ker je * 10
+
+end1 = "C" #end1, end2 etc. so razlicni variables 
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6, 
+print end7 + end8 + end9 + end10 + end11 + end12
+
+#ce zbrises vejico napise Cheese v svojo vrstico in Burger v svojo. ce pa je z vejico pa napise Cheese Burger skupaj v isti vrstici.
