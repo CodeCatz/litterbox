4137162a5cc36f962e74983e1e20238d29a26a80
Ajda Rotar <ajda.rotar@gmail.com>
2013-07-31 17:56:00 +0200

    ajda hw

---
+++ homework ajda/ex1.py
@@ -0,0 +1,9 @@
+#print "Hello World!"
+#print "Hello Again"
+#print "I link typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+#print "another line"
+print "only one line printed"
