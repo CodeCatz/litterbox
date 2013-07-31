af7dd880c00d2a48c4496ed767e7415f5a62f33a
zigomir <zigomir@gmail.com>
2013-07-31 19:44:02 +0200

    post method

--- web.py
+++ web.py
@@ -1,11 +1,14 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('test.html', name="Metallica")
+	return render_template('test.html', name="Metallica")
+
+@app.route('/post', methods=['POST'])
+def post_test():
+	return request.form['test']
 
 if __name__ == "__main__":
     app.run(debug=True)
