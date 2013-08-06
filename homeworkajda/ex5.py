8b6f529d991d431277086a8811e9abbfb60cfacc
Ajda Rotar <ajda.rotar@gmail.com>
2013-08-06 23:08:47 +0200

    homeworkajda

---
+++ homeworkajda/ex5.py
@@ -0,0 +1,21 @@
+my_name = 'Ajda'
+my_age = 35 #not a lie
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+#za tekst das %s, za stevilke pa %d
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall" % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+#tricky line
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+print "lets see what r does %r, and %r" % (my_name, my_age)
