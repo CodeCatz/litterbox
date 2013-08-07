9764941d80fec0194884945cdb70dec0f48c347e
Alja Isakovic <ialja@me.com>
2013-08-07 21:31:08 +0200

    flask app with bootstrap, woot

--- Alja/Better-Flask/server.py
+++ Alja/Better-Flask/server.py
@@ -3,15 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# just another test page
+# just a simple test page
 @app.route('/test')
 def test():
 	return "This is just a test."
 
 @app.route('/cats')
-def demo():
+def cats():
 	return render_template('cats.html')
 
+@app.route('/hipster')
+def hipster():
+	return render_template('hipster.html')
+
+@app.route('/grumpy')
+def grumpy():
+	return render_template('grumpy.html')
+
+@app.route('/faq')
+def faq():
+	return render_template('faq.html')
 
 # defining root page
 @app.route('/')
