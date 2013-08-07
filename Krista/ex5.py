8e6be41f2f66af797cf807ccc1f6ea1407cc43eb
Krista Krzan <betmenka@gmail.com>
2013-08-07 18:42:06 +0200

    Dodajam vaje 4-8

---
+++ Krista/ex5.py
@@ -0,0 +1,21 @@
+ # -*- coding: utf-8 -*-
+ 
+my_name = 'Krista Keržan'
+my_age = 32 # not a lie
+my_height = 161 # cm
+my_weight = 53 # kg
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair = 'brown'
+
+
+print "Let's talk about %s." % my_name
+print "He's %d cm tall." % my_height
+print "He's %d kilos heavy." % my_weight
+print "Actaually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the cofee." % my_teeth
+
+#this line is tricky, try to get it exactly right
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
---
+++ Krista/ex7.py
@@ -0,0 +1,26 @@
+print "Mary had a little lamb." # prints this sentence
+print "Its fleece was white as %s." % 'snow' # print this sentence with string snow instead of %s
+# its not a variable
+
+print "And everywhere that Mary went."
+print "." * 10 #what'd that do? # ad 10 commas 
+
+end1 = "C" # end variabile is just happen to have the word "end"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# whatch that commma at the end. try removing it to see what happens 
+
+print end1 + end2 + end3 + end4 + end5 + end6, # prints Cheese
+print end7 + end8 + end9 + end10 + end11 + end12 # prints Burger
+
+# comma is so the line is no longer than 80 characters, which in Python is bad style. 
