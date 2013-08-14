acd7f441b185b0553554ed1a04a16a06116a3733
Alja Isakovic <ialja@me.com>
2013-08-14 19:53:46 +0200

    another flask app

---
+++ Alja/DogzNCatz/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/photo')
+def photo():
+    return render_template('photo.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
