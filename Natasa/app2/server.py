9c8573d7522ad87ad0d17947d9c26d5e00902450
Fibiola <natasha.pristovsek@gmail.com>
2013-08-21 23:27:03 +0200

    Complexity of life z Bootstrapom

--- Natasa/app2/server.py
+++ Natasa/app2/server.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-#render template bo vzel te nase template in jih zdruzil 
+#render template bo vzel te nase template in jih zdruzil
 
 app = Flask(__name__)
 #ko bomo poklicali app kr direktno v root --> "/" dobimo tole
 @app.route('/')
 def home():
 	return render_template("home.html")
-#povemo naj zrendrira template in povemo tud kterga 
+#povemo naj zrendrira template in povemo tud kterga
 
 @app.route('/demo')
 def demo():
@@ -15,6 +15,8 @@ def demo():
 @app.route('/about')
 def about():
 	return render_template("about.html")
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
