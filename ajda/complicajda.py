cad5b3640b6e0519aee102d4bcde4f3af48e2825
Ajda Rotar <ajda.rotar@gmail.com>
2013-08-28 19:25:03 +0200

    test

--- ajda/complicajda.py
+++ ajda/complicajda.py
@@ -1,18 +1,18 @@
 # 1. del: funkcije
 
-#gender: female = 5, male = 0
+#gender: female = 2, male = 0
 
 def calculate_score_for_gender(gender):
 	if gender == "male":
 		return 0
-	else: return 5
+	else: return 2
 
 
-#age: 0-100 if age < 10 --> 0, 11 < age < 20 --> 4, 21 < age < 35 --> 2, 36 < age < 50 --> 4, 50+ --> 1
+#age: 0-100 if age < 10 --> 0, 11 < age < 20 --> 5, 21 < age < 35 --> 2, 36 < age < 50 --> 4, 50+ --> 1
 
 def calculate_score_for_age(age):
 	if (age > 11 and age  <= 20) or (age > 36 and age <= 50):
-		return 4
+		return 5
 	
 	elif age > 20 and age <= 35:
 		return 2
@@ -39,7 +39,7 @@ def calculate_score_for_status(status):
 		return 3
 
 	elif status == "I'm a pizza":
-		return 4
+		return 0
 
 	else:
 		return 5
@@ -48,21 +48,21 @@ def calculate_score_for_status(status):
 # ignorance: 0 = Problem is my challenge, 1 = Who gives a fuck, 2 = I'm an angel
 
 def calculate_score_for_ignorance(ignorance):
-	if ignorance == "problem is my challenge":
+	if ignorance == "Ignorance is bliss":
 		return 0
 
-	elif ignorance == "who gives a fuck":
-		return 1
-
-	else:
+	elif ignorance == "not at all":
 		return 2
+
+	elif ignorance == "I'm an angel":
+		return 4
 
 
 # money_have: -10000+ = 6, (-10000)-(-5000) = 5, -5000-0 = 4, 0-500 = 3, 500-3000 = 2, 3000-10000 = 1, 10000+ = 0
 
 def calculate_score_for_money_have(money_have):
 	if money_have <= (-10000.0):
-		return 6.0
+		return 8.0
 
 	elif money_have > (-10000.0) and money_have <= (-5000.0):
 		return 5.0
@@ -102,16 +102,56 @@ def caluculate_score_for_money_want(money_want):
 		return 5
 
 
+#real friends: 0 = 5, 1-3 = 1, 4-6 = 2, 7-9 = 3, 10+ = 4
+
+def calculate_score_for_rl_friends(rl_friends):
+	if rl_friends == 0:
+		return 5
+
+	elif rl_friends >= 1 and rl_friends <= 3:
+		return 1
+
+	elif rl_friends >= 4 and rl_friends <= 6:
+		return 2
+
+	elif rl_friends >= 7 and rl_friends <= 9:
+		return 3
+
+	else:
+		return 4
+
+
+#children: 0 = 1, 1-2 = 2, 3 = 3, 4 = 4, 5+ = 5
+
+def calculate_score_for_children(children):
+	if children == 0: 
+		return 1
+
+	elif children == 1 and children == 2:
+		return 2
+
+	elif children == 3:
+		return 3
+
+	elif children == 4:
+		return 4
+
+	else:
+		return 5
+
+
 
 # 2. del: sestevek funkcij
 
-def calculate_score(gender, age, status, ignorance, money_have, money_want):
+def calculate_score(gender, age, status, ignorance, money_have, money_want, rl_friends, children):
 	result = calculate_score_for_gender(gender)
 	result += calculate_score_for_age(age)
 	result += calculate_score_for_status(status)
 	result += calculate_score_for_ignorance(ignorance)
 	result += calculate_score_for_money_have(money_have)
 	result += caluculate_score_for_money_want(money_want)
+	result += calculate_score_for_rl_friends(rl_friends)
+	result += calculate_score_for_children(children)
 	return result
 
 
@@ -155,8 +195,23 @@ money_want = float(raw_input(">> "))
 while money_want < 0: #---->zato, da je pozitivno stevilo!
 	money_want = float(raw_input("I didn't ask for apples and peaches. So, how much money do you want? "))
 
+#rl_friends
+print "How many real friends have you got?"
+rl_friends = raw_input(">> ")
+while not rl_friends.isdigit():
+	rl_friends = raw_input("Spock doesn't count. Think again - how many? ")
+
+#children
+print "How many children have you got?"
+children = raw_input(">> ")
+while not children.isdigit():
+	children = raw_input("No aliens, just humans, please: ")
+
+
+
+
 # 4.del: sestevek
 
-print "Kompliciranost tvojega zivljenja je: ", calculate_score(gender, int(age), status, ignorance, money_have, money_want)
+print "On a scale from 0 to 40, your life complication is : ", calculate_score(gender, int(age), status, ignorance, money_have, money_want, rl_friends, children)
 
 
